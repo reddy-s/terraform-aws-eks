9f97ef86cf89491557da5adf29fd197f4a477f76
Anders Soder <anders.soder@kindredgroup.com>
2019-04-10 11:48:59 +0200

    tag handling tests

--- workers_launch_template.tf
+++ workers_launch_template.tf
@@ -1,15 +1,4 @@
 # Worker Groups using Launch Templates
-module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.2.1"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  delimiter  = "${var.delimiter}"
-  attributes = ["${compact(concat(var.attributes, list("workers")))}"]
-  tags       = "${var.tags}"
-  enabled    = "${var.enabled}"
-}
-
 resource "aws_cloudformation_stack" "workers_launch_template" {
   count = "${var.enabled == "true" ? var.worker_group_launch_template_count : 0}"
 
@@ -23,7 +12,7 @@ resource "aws_cloudformation_stack" "workers_launch_template" {
     map("key", "k8s.io/cluster-autoscaler/${aws_eks_cluster.this.name}", "value", "", "propagate_at_launch", false),
     map("key", "k8s.io/cluster-autoscaler/node-template/resources/ephemeral-storage", "value", "${lookup(var.worker_groups_launch_template[count.index], "root_volume_size", local.workers_group_launch_template_defaults["root_volume_size"])}Gi", "propagate_at_launch", false),
     map("key", "EKS", "value", "true", "propagate_at_launch", true),
-    module.label.tags,
+    var.tags,
     var.worker_group_launch_template_tags[contains(keys(var.worker_group_launch_template_tags), "${lookup(var.worker_groups_launch_template[count.index], "name", count.index)}") ? "${lookup(var.worker_groups_launch_template[count.index], "name", count.index)}" : "default"]
   )}"
 
